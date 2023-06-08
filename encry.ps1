b08108b4892a7e3a39a4c5b990e138242999f286
prophecyun <prophecyun@gmail.com>
2023-06-08 21:28:34 +0800

    Update encry.ps1

--- encry.ps1
+++ encry.ps1
@@ -1,13 +1,15 @@
-﻿ Set-ExecutionPolicy unrestricted
+#Set-ExecutionPolicy unrestricted
  $file = "c:\users\user\desktop\test.zip"
  $DestinationFile = "c:\users\user\desktop\encry"
 
-$FileStreamReader = New-Object System.IO.FileStream($File, [System.IO.FileMode]::Open)
-$FileStreamWriter = New-Object System.IO.FileStream($DestinationFile, [System.IO.FileMode]::Create)
+ $FileStreamReader = New-Object System.IO.FileStream($File, [System.IO.FileMode]::Open)
+ $FileStreamWriter = New-Object System.IO.FileStream($DestinationFile, [System.IO.FileMode]::Create)
 
 #create encryption key
- $EncryptionKeyBytes = New-Object Byte[] 32 | out-file C:\users\user\Desktop\key
-
+ $EncryptionKeyBytes = New-Object Byte[] 32
+ [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($EncryptionKeyBytes)
+ $EncryptionKeyBytes | out-file C:\users\user\Desktop\key
+ $Algorithm = 'AES'
  $Crypto = [System.Security.Cryptography.SymmetricAlgorithm]::Create($Algorithm)
 
  $Crypto.key = Get-Content C:\users\user\Desktop\key
